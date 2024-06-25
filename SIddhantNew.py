b4c7289ff3d0fdd0ad4687f88e6ae576ab4c83ae
Siddhant1609 <siddhantsingh16092000@gmail.com>
2024-06-25 13:20:24 +0530

    File Added for git Shortcut

---
+++ SIddhantNew.py
@@ -0,0 +1,8 @@
+print("Siddhant Singh")
+
+
+# this file contain the data of git Learinigs for future
+# git  status     --> to check the updates in the Repo.
+# git add .       --> add the Updates changes for commit into the Repo.
+# git commit -m "Any Comment u Like"    --> provide  comit for identification
+# git push https://github.com/Siddhant1609/Pythonpractice.git        -->to push the updates Repo on given Git-Repo Link
